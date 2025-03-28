b4131465fa8a9551557105c8621c1931cd2cdd10
hejl <jlhe@infosky.com.cn>
2025-03-28 14:16:00 +0800

    Revert "尝试优化弹幕播放卡顿"
    This reverts commit 412a1ad135719540b02f1f2f71a9eb9d30b93424.

--- plugins.v2/danmu/__init__.py
+++ plugins.v2/danmu/__init__.py
@@ -26,7 +26,7 @@ class Danmu(_PluginBase):
     # 主题色
     plugin_color = "#3B5E8E"
     # 插件版本
-    plugin_version = "1.1.15"
+    plugin_version = "1.1.14"
     # 插件作者
     plugin_author = "edhnt455"
     # 作者主页
